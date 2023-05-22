781c07adf8a1ad6c3c0714289f7ab4986154250f
edwin <ngisahedwin@gmail.com>
2023-05-22 22:16:06 +0300

    login form validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState} from 'react'
 import {Link} from 'react-router-dom'
 
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
+
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs)
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
   return (
     <div className='auth'>
         <h1>Login</h1>
