d8c76bd2ea5982ef001996705b380dc85c116bea
edwin <ngisahedwin@gmail.com>
2023-05-24 19:05:13 +0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const Home = () => {
-  const posts = [
-    {
-      "id": "1",
-      "title": "lorem Ipsum",
-      "desc": "This is sample data #1",
-      "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
-  },
-  {
-    "id": "2",
-    "title": "lorem Ipsum",
-    "desc": "This is sample data #1",
-    "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
-},
-{
-  "id": "3",
-  "title": "lorem Ipsum",
-  "desc": "This is sample data #1",
-  "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
-},
-{
-  "id": "4",
-  "title": "lorem Ipsum",
-  "desc": "This is sample data #1",
-  "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
-},
-{
-  "id": "5",
-  "title": "lorem Ipsum",
-  "desc": "This is sample data #1",
-  "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
-},
 
-  ];
+  const [posts, setPosts] = useState([])
+
+  const cat = useLocation().search;
+
+  useEffect(() =>{
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  },
+  [cat]
+
+  )
+//   const posts = [
+//     {
+//       "id": "1",
+//       "title": "lorem Ipsum",
+//       "desc": "This is sample data #1",
+//       "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
+//   },
+//   {
+//     "id": "2",
+//     "title": "lorem Ipsum",
+//     "desc": "This is sample data #1",
+//     "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
+// },
+// {
+//   "id": "3",
+//   "title": "lorem Ipsum",
+//   "desc": "This is sample data #1",
+//   "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
+// },
+// {
+//   "id": "4",
+//   "title": "lorem Ipsum",
+//   "desc": "This is sample data #1",
+//   "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
+// },
+// {
+//   "id": "5",
+//   "title": "lorem Ipsum",
+//   "desc": "This is sample data #1",
+//   "img": "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1342&q=80"
+// },
+
+//   ];
   return (
     <div className='home'>
       <div className='posts'>
